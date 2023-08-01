54aec6035237f0b53653a50b7f17ad8acd7de8b9
MuratYucedag <yucedagmurat23@gmail.com>
2023-08-01 21:26:18 +0300

    Add project files.

---
+++ MyEgitimAkademi_Portfolio/Controllers/EducationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEgitimAkademi_Portfolio.Controllers
+{
+    public class EducationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyEgitimAkademi_Portfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyEgitimAkademi_Portfolio.MvcApplication" Language="C#" %>
